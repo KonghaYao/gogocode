97f56afa8f870a2dc9282a2088b3a8ef54a20a5d
3446798488 <3446798488@qq.com>
2024-09-19 13:21:43 +0000

    feat: 添加新构建脚本

--- packages/gogocode-core/rollup.config.mjs
+++ packages/gogocode-core/rollup.config.mjs
@@ -3,14 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
-const commonPlugins = [
-    json(),
-    commonjs(),
-    resolve({ preferBuiltins: false, browser: true }),
-    nodePolyfills(),
-];
+const commonPlugins = [json(), commonjs(), resolve({ preferBuiltins: false, browser: true }), nodePolyfills()];
 
 export default [
     // browser-friendly UMD build
